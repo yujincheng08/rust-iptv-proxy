28b66203af3d89bf7ba5caca43119289c9c18bfc
LoveSy <shana@zju.edu.cn>
2024-02-15 02:09:45 +0800

    Fix udp proxy when multi clients on the same addr

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,20 +1,34 @@
-use std::net::{Ipv4Addr, SocketAddrV4};
-
 use actix_web::web::Bytes;
 use anyhow::Result;
 use async_stream::stream;
 use futures_core::stream::Stream;
 use futures_util::stream::StreamExt;
+use lazy_static::lazy_static;
 #[cfg(not(any(target_os = "android", target_os = "fuchsia", target_os = "linux")))]
 use local_ip_address::list_afinet_netifas;
 use log::{error, info};
 use reqwest::Url;
 use retina::client::{PacketItem, Session, SessionOptions};
 use rtp_rs::RtpReader;
-use tokio::{net::UdpSocket, sync::mpsc};
-use tokio_util::bytes::Buf;
-use tokio_util::codec::BytesCodec;
-use tokio_util::udp::UdpFramed;
+use std::collections::HashMap;
+use std::net::{Ipv4Addr, SocketAddrV4};
+use std::sync::Mutex;
+use tokio::{
+    net::UdpSocket,
+    sync::{
+        broadcast::{
+            self,
+            error::RecvError,
+            Sender,
+        },
+        mpsc::{self},
+    },
+};
+use tokio_util::{
+    bytes::{Buf, BytesMut},
+    codec::BytesCodec,
+    udp::UdpFramed,
+};
 
 fn filter_reordered_seq(seq: &mut u16, next: u16) -> bool {
     let valid = seq.wrapping_add(3000);
@@ -80,8 +94,7 @@ pub(crate) fn rtsp(url: String, if_name: Option<String>) -> impl Stream<Item = R
         });
 
         loop {
-            let stream = rx.recv().await;
-            if let Some(stream) = stream {
+            if let Some(stream) = rx.recv().await {
                 yield Ok(stream);
             } else {
                 error!("Connection closed");
@@ -91,59 +104,87 @@ pub(crate) fn rtsp(url: String, if_name: Option<String>) -> impl Stream<Item = R
     }
 }
 
-pub(crate) fn udp(multi_addr: SocketAddrV4) -> impl Stream<Item = Result<Bytes>> {
-    stream! {
-        #[cfg(target_os = "windows")]
-        let socket =  {
-            let socket = socket2::Socket::new(
-                socket2::Domain::IPV4,
-                socket2::Type::DGRAM,
-                Some(socket2::Protocol::UDP),
-            )?;
-            socket.set_reuse_address(true)?;
-            socket.bind(&SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), multi_addr.port()).into())?;
-            UdpSocket::from_std(socket.into())?
-        };
-        #[cfg(not(target_os = "windows"))]
-        let socket = {
-            UdpSocket::bind(multi_addr).await?
-        };
-        socket.set_multicast_loop_v4(true)?;
+lazy_static! {
+    static ref UDP_CHANNELS: Mutex<HashMap<SocketAddrV4, Sender<BytesMut>>> =
+        Mutex::new(HashMap::new());
+}
 
-        socket.join_multicast_v4(
-            multi_addr.ip().clone(),
-            Ipv4Addr::new(0, 0, 0, 0),
+async fn join_multicast(multi_addr: SocketAddrV4, tx: Sender<BytesMut>) -> Result<()> {
+    #[cfg(target_os = "windows")]
+    let socket = {
+        let socket = socket2::Socket::new(
+            socket2::Domain::IPV4,
+            socket2::Type::DGRAM,
+            Some(socket2::Protocol::UDP),
         )?;
+        socket.set_reuse_address(true)?;
+        socket.bind(&SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), multi_addr.port()).into())?;
+        UdpSocket::from_std(socket.into())?
+    };
+    #[cfg(not(target_os = "windows"))]
+    let socket = { UdpSocket::bind(multi_addr).await? };
+    socket.set_multicast_loop_v4(true)?;
 
-        info!("Udp proxy joined {}", multi_addr);
+    socket.join_multicast_v4(multi_addr.ip().clone(), Ipv4Addr::new(0, 0, 0, 0))?;
 
-        let mut steram = UdpFramed::new(socket, BytesCodec::new());
-        let (tx, mut rx) = mpsc::channel(128);
+    info!("Udp proxy joined {}", multi_addr);
 
-        tokio::spawn(async move {
-            let mut seq = 0u16;
-            while let Some(item) = steram.next().await {
-                if let Ok((mut bytes, _)) = item {
-                    if let Ok(rtp) = RtpReader::new(bytes.as_ref()) {
-                        let next = rtp.sequence_number().into();
-                        bytes.advance(rtp.payload_offset());
-                        if !filter_reordered_seq(&mut seq, next) || tx.send(bytes).await.is_ok() {
-                            continue;
-                        }
-                    }
+    let mut stream = UdpFramed::new(socket, BytesCodec::new());
+
+    let mut seq = 0u16;
+    while let Some(item) = stream.next().await {
+        if let Ok((mut bytes, _)) = item {
+            if let Ok(rtp) = RtpReader::new(bytes.as_ref()) {
+                let next = rtp.sequence_number().into();
+                bytes.advance(rtp.payload_offset());
+                if filter_reordered_seq(&mut seq, next) &&
+                    tx.send(bytes).map(|cnt| cnt > 0).unwrap_or(false) {
+                    continue;
                 }
-                break;
-            }
-        });
-
-        loop {
-            let stream = rx.recv().await;
-            if let Some(stream) = stream {
-                yield Ok(Bytes::from(stream));
-            } else {
-                error!("Connection closed");
-                break;
             }
         }
+        info!("Udp proxy left {}", multi_addr);
+        stream
+            .get_mut()
+            .leave_multicast_v4(multi_addr.ip().clone(), Ipv4Addr::new(0, 0, 0, 0))
+            .ok();
+        UDP_CHANNELS.lock().unwrap().remove(&multi_addr);
+        break;
+    }
+
+    Ok(())
+}
+
+pub(crate) fn udp(multi_addr: SocketAddrV4) -> impl Stream<Item = Result<Bytes>> {
+    let (tx, mut rx) = {
+        let mut channels = UDP_CHANNELS.lock().unwrap();
+        match channels.get_mut(&multi_addr) {
+            Some(tx) => (None, tx.subscribe()),
+            None => {
+                let (tx, rx) = broadcast::channel(128);
+                channels.insert(multi_addr, tx.clone());
+                (Some(tx), rx)
+            }
+        }
+    };
+    stream! {
+        if let Some(tx) = tx {
+            tokio::spawn(join_multicast(multi_addr, tx));
+        }
+        loop {
+            match rx.recv().await {
+                Ok(stream) =>  {
+                    yield Ok(Bytes::from(stream));
+                },
+                Err(RecvError::Lagged(_)) => {
+                    rx = rx.resubscribe()
+                },
+                Err(RecvError::Closed) => {
+                    error!("Connection closed");
+                    break;
+                }
+            }
+        }
+        yield Ok(Bytes::from("hello"))
     }
 }
